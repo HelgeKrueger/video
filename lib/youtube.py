686f8326ce2cc162d8690efa13b9c0d166c54fad
Helge <helge.krueger@gmail.com>
2019-06-10 11:01:34 +0200

    [Task-6] Script to upload to youtube

--- lib/youtube.py
+++ lib/youtube.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.videos.insert
-# NOTES:
-# 1. This sample code uploads a file and can't be executed via this interface.
-#    To test this code, you must run it locally using your own API credentials.
-#    See: https://developers.google.com/explorer-help/guides/code_samples#python
-# 2. This example makes a simple upload request. We recommend that you consider
-#    using resumable uploads instead, particularly if you are transferring large
-#    files or there's a high likelihood of a network interruption or other
-#    transmission failure. To learn more about resumable uploads, see:
-#    https://developers.google.com/api-client-library/python/guide/media_upload
-
 import httplib2
 import os
 
@@ -26,72 +13,65 @@ from oauth2client.tools import run_flow
 scopes = ["https://www.googleapis.com/auth/youtube.upload"]
 
 
-def get_credentials():
-    client_secrets_file = "./secrets/client_secret_740234423024-m2hdv91nbse10mfldt162njrd9matut0.apps.googleusercontent.com.json"
+class YouTube:
+    def __init__(self, storage_file='./secrets/credentials.storage'):
+        self.storage_file = storage_file
+        self.youtube = None
 
-    storage = Storage('./secrets/credentials.storage')
-    credentials = storage.get()
+    def get_credentials(self):
+        client_secrets_file = "./secrets/client_secret_740234423024-m2hdv91nbse10mfldt162njrd9matut0.apps.googleusercontent.com.json"
 
-    if credentials is None or credentials.invalid:
-        flow = flow_from_clientsecrets(client_secrets_file, scope=scopes[0])
-        credentials = run_flow(flow, storage, http=httplib2.Http())
+        storage = Storage(self.storage_file)
+        credentials = storage.get()
 
-    return credentials
+        if credentials is None or credentials.invalid:
+            flow = flow_from_clientsecrets(client_secrets_file, scope=scopes[0])
+            credentials = run_flow(flow, storage, http=httplib2.Http())
 
+        self.credentials = credentials
 
-def upload_file(youtube, filename, title, description):
-    upload_body = {
-        "snippet": {
-            "categoryId": "22",
-            "description": description,
-            "title": title,
-        },
-        "status": {
-            "privacyStatus": "public"
+    def init(self):
+        # Disable OAuthlib's HTTPS verification when running locally.
+        # *DO NOT* leave this option enabled in production.
+        os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+        api_service_name = "youtube"
+        api_version = "v3"
+
+        # Get credentials and create an API client
+        self.get_credentials()
+        self.youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, credentials=self.credentials)
+
+    def upload_file(self, filename, title, description):
+        upload_body = {
+            "snippet": {
+                "categoryId": "22",
+                "description": description,
+                "title": title,
+            },
+            "status": {
+                "privacyStatus": "public"
+            }
         }
-    }
-    media = MediaFileUpload(filename, resumable=True)
+        media = MediaFileUpload(filename, resumable=True)
 
-    try:
-        request = youtube.videos().insert(
-            part="snippet,status",
-            body=upload_body,
-            media_body=media
-        )
-        response = None
-        # response = request.execute()
-        while response is None:
-            status, response = request.next_chunk()
-            if status:
-                print("Uploaded %d%%." % int(status.progress() * 100))
-        print("Upload Complete!")
-        print(response)
-    except googleapiclient.errors.HttpError as err:
-        print(err)
-        print(err.content)
-        print(err.error_details)
+        try:
+            request = self.youtube.videos().insert(
+                part="snippet,status",
+                body=upload_body,
+                media_body=media
+            )
+            response = None
+            # response = request.execute()
+            while response is None:
+                status, response = request.next_chunk()
+                if status:
+                    print("Uploaded %d%%." % int(status.progress() * 100))
+            print("Upload Complete!")
+            print(response)
+        except googleapiclient.errors.HttpError as err:
+            print(err)
+            print(err.content)
+            print(err.error_details)
 
-
-def create_youtube():
-    api_service_name = "youtube"
-    api_version = "v3"
-
-    # Get credentials and create an API client
-    credentials = get_credentials()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
-
-    return youtube
-
-
-def main():
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
-    youtube = create_youtube()
-    upload_file(youtube, "output/tmp.mp4", "open road", "A strech of open road")
-
-
-if __name__ == "__main__":
-    main()
---
+++ tools/upload_video.py
@@ -0,0 +1,15 @@
+import argparse
+
+from lib.youtube import YouTube
+
+parser = argparse.ArgumentParser(description='Uploads video to youtube')
+parser.add_argument('--input', help='video to upload', default='output/tmp.mp4')
+parser.add_argument('--title', help='title of video')
+parser.add_argument('--description', help='description of video')
+
+args = parser.parse_args()
+
+youtube = YouTube()
+youtube.init()
+
+youtube.upload_file(args.input, args.title, args.description)
