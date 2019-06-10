109fa87c9179450678488a29caab120e3b1791f7
Helge <helge.krueger@gmail.com>
2019-06-10 10:50:28 +0200

    [Task-14] Add length parameter

--- tools/extract_subclip.py
+++ tools/extract_subclip.py
@@ -7,9 +7,16 @@ parser.add_argument('--input', help='video stream')
 parser.add_argument('--output', help='output video file', default='output/tmp.mp4')
 parser.add_argument('--start', help='start time in seconds',  type=float)
 parser.add_argument('--end', help='end time in seconds',  type=float)
+parser.add_argument('--length', help='length of clip',  type=float, default=60)
 
 args = parser.parse_args()
 clip = VideoFileClip(args.input)
 
-subclip = clip.without_audio().subclip(t_start=args.start, t_end=args.end)
+start_time = args.start
+end_time = args.end
+
+if end_time is None:
+    end_time = start_time + args.length - 1
+
+subclip = clip.without_audio().subclip(t_start=start_time, t_end=end_time)
 subclip.write_videofile(args.output)
