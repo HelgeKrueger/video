ca6d24c1fc3be2712c5e8b58a004972dcf735387
Helge <helge.krueger@gmail.com>
2020-01-24 12:52:49 +0100

    tests on github

--- lib/data/__init__.py
+++ lib/data/__init__.py
@@ -1,5 +1,8 @@
 from .video_file_segments import VideoFileSegments
-from .video_file import VideoFile
 from .meta_information import MetaInformation
 from .processing_information import ProcessingInformation
 from .object_data_for_video import ObjectDataForVideo
+try:
+    from .video_file import VideoFile
+except Exception:
+    print("No ffmpeg installed, not all features will be available")
